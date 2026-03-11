1abdeec3545aa926e6992599b1dea3218ff6a7f9
Omkar Magadum <omkar.magadum.dev@gmail.com>
2026-03-11 10:29:07 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App(){
+  return(
+    <>
+      <h1>Lets build our first project in react </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, voluptates?</p>     
+    </>
+  )
+}
